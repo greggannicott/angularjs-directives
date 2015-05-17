74701b1cfe94385163087a24d28bb0d7c65797b9
Greg Gannicott <greg@gannicott.co.uk>
2015-05-17 14:45:08 +0000

    TreeView: Checkbox support. Note, doesn't not include 'select all' functionality.

--- gg-tree-view/ggTreeView.js
+++ gg-tree-view/ggTreeView.js
@@ -3,10 +3,29 @@ angular.module('gg.directives')
         return {
             restrict: "E",
             scope: {
-                treeData: "="
+                treeData: "=",
+                displayCheckboxes: "="
             },
             templateUrl: "ggTreeView.html",
+            link: function(scope, element, attributes) {
+
+                // Determine if we're going to display checkboxes or not
+                if (scope.displayCheckboxes !== "undefined" && scope.displayCheckboxes === true) {
+                    console.log("yes");
+                    scope.areCheckboxesDisplayed = true;
+                }
+                else {
+                    scope.areCheckboxesDisplayed = false;
+                    console.log("no");
+                }
+
+            },
             controller: function($scope) {
+
+                //var getStateOfChildNodes;
+
+                $scope.areCheckboxesDisplayed = false;
+
                 $scope.getNodeIcon = function(node) {
                     if (typeof node.isExpanded !== "undefined" && node.isExpanded === true) {
                         return "fa-caret-down";
@@ -35,15 +54,128 @@ angular.module('gg.directives')
                     $scope.deselectAllNodes($scope.treeData);
                     node.isSelected = !node.isSelected;
                 }
-                
+
                 $scope.deselectAllNodes = function(nodes) {
                     $.each(nodes, function() {
-                        if (typeof this.childNodes !== 'undefined' && this.childNodes.length > 0) {
+                        if (hasChildNodes(this)) {
                             $scope.deselectAllNodes(this.childNodes);
                         }
                         this.isSelected = false;
                     });
                 }
+                
+                
+                $scope.toggleCheckbox = function(node) {
+                    // if (hasChildNodes(node)) {
+                    //     var stateOfDescendantNodes = getStateOfDescendantNodes(node);
+                    //     console.log(stateOfDescendantNodes);
+                    //     if (stateOfDescendantNodes === "none") {
+                    //         node.isChecked = true;
+                    //         checkAllDescendantNodes(node);
+                    //     }
+                    //     else if (stateOfDescendantNodes === "mixed") {
+                    //         node.isChecked = false;
+                    //         uncheckAllDescendantNodes(node);
+                    //     }
+                    //     else {
+                    //         node.isChecked = false;
+                    //         uncheckAllDescendantNodes(node);
+                    //     }
+                    // }
+                    // else {
+                        node.isChecked = !node.isChecked;
+                    // }
+                }
+
+                $scope.getCheckboxIcon = function(node) {
+                    // // If it has children, check the state of them
+                    // if (hasChildNodes(node)) {
+                    //     var stateOfDescendantNodes = getStateOfDescendantNodes(node);
+                    //     if (stateOfDescendantNodes === "none") {
+                    //         return "fa-circle-o";
+                    //     }
+                    //     else if (stateOfDescendantNodes === "mixed") {
+                    //         return "fa-dot-circle-o";
+                    //     }
+                    //     else {
+                    //         return "fa-check-circle-o";
+                    //     }
+                    // }
+                    // else {
+                        if (node.isChecked === true) {
+                            return "fa-check-circle-o";
+                        }
+                        else {
+                            return "fa-circle-o";
+                        }
+                    // }
+                }
+
+                // var getStateOfDescendantNodes = function(node) {
+                //     var totalNumberOfDescendantNodes = getTotalNumberOfDescendantNodes(node);
+                //     var totalNumberOfDescendantNodesChecked = getTotalNumberOfDescendantNodesChecked(node);
+                    
+                //     // console.log("Name", node.displayText);
+                //     // console.log("totalNumberOfDescendantNodes",totalNumberOfDescendantNodes);
+                //     // console.log("totalNumberOfDescendantNodesChecked",totalNumberOfDescendantNodesChecked);
+
+                //     if (totalNumberOfDescendantNodesChecked === 0) {
+                //         return "none";
+                //     }
+                //     else if (totalNumberOfDescendantNodesChecked > 0 && totalNumberOfDescendantNodesChecked < totalNumberOfDescendantNodes) {
+                //         return "mixed";
+                //     }
+                //     else {
+                //         return "all";
+                //     }
+                // }
+
+                // var getTotalNumberOfDescendantNodes = function(node) {
+                //     var total = 0;
+                //     $.each(node.childNodes, function() {
+                //         if (hasChildNodes(this)) {
+                //             total += getTotalNumberOfDescendantNodes(this);
+                //         }
+                //         total++;
+                //     });
+                //     return total;
+                // }
+
+                // var getTotalNumberOfDescendantNodesChecked = function(node) {
+                //     var total = 0;
+                //     $.each(node.childNodes, function() {
+                //         if (hasChildNodes(this)) {
+                //             total += getTotalNumberOfDescendantNodesChecked(this);
+                //         }
+                //         if (this.isChecked === true) {
+                //             total++;
+                //         }
+                //     });
+                //     return total;
+                // }
+
+
+                // var checkAllDescendantNodes = function(node) {
+                //     $.each(node.childNodes, function() {
+                //         if (hasChildNodes(this)) {
+                //             checkAllDescendantNodes(this);
+                //         }
+                //         this.isChecked = true;
+                //     })
+                // }
+
+                // var uncheckAllDescendantNodes = function(node) {
+                //     $.each(node.childNodes, function() {
+                //         if (hasChildNodes(this)) {
+                //             uncheckAllDescendantNodes(this);
+                //         }
+                //         this.isChecked = false;
+                //     })
+                // }
+
+                // var hasChildNodes = function(node) {
+                //     return (typeof node.childNodes !== "undefined" && node.childNodes.length > 0)
+                // }
 
             }
         }
--- gg-tree-view/script.js
+++ gg-tree-view/script.js
@@ -2,21 +2,29 @@ angular.module('gg').controller('demoController', function($scope) {
     $scope.treeData = [
         {"id": "1", "displayText": "Entry 1", "isExpanded":true, "childNodes":
             [
-            {"id": "1.1", "displayText": "Entry 1.1" },
-            {"id": "1.1", "displayText": "Entry 1.1", "isExpanded": true, "isSelected": true, "childNodes": [
-                {"id": "1.1.1", "displayText": "Entry 1.1.1"},
-                {"id": "1.1.2", "displayText": "Entry 1.1.2"}
+            {"id": "1.1", "displayText": "Entry 1.1", "isChecked": true },
+            {"id": "1.2", "displayText": "Entry 1.2", "isExpanded": true, "isSelected": true, "childNodes": [
+                {"id": "1.2.1", "displayText": "Entry 1.2.1", "childNodes": [
+                    {"id": "1.2.1.1", "displayText": "Entry 1.2.1.1"},
+                    {"id": "1.2.2.2", "displayText": "Entry 1.2.1.2", "childNodes": [
+                        {"id": "1.2.2.1", "displayText": "Entry 1.2.2.1"}
+                        ]
+                    },
+                    ]
+                },
+                {"id": "1.2.2", "displayText": "Entry 1.2.2"}
                 ]
             },
-            {"id": "1.1", "displayText": "Entry 1.1"},
+            {"id": "1.3", "displayText": "Entry 1.3"},
+            {"id": "1.4", "displayText": "Entry 1.4"},
             ]
         },
-        {"id": "2", "displayText": "Entry 2"},
-        {"id": "3", "displayText": "Entry 3", "childNodes": [
-            {"id": "3.1", "displayText": "Entry 3.1"}
-            ]
-        },
-        {"id": "4", "displayText": "Entry 4"},
-        {"id": "5", "displayText": "Entry 5"}
+        // {"id": "2", "displayText": "Entry 2"},
+        // {"id": "3", "displayText": "Entry 3", "childNodes": [
+        //     {"id": "3.1", "displayText": "Entry 3.1"}
+        //     ]
+        // },
+        // {"id": "4", "displayText": "Entry 4"},
+        // {"id": "5", "displayText": "Entry 5"}
         ];
 });
