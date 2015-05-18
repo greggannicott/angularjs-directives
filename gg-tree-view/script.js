da36305834e6f492ffc728e85875cc1ffe776e7a
Greg Gannicott <greg@gannicott.co.uk>
2015-05-18 19:18:39 +0000

    TreeView - 'Mixed' state checkboxes are now set when the directive is initiated.

--- gg-tree-view/script.js
+++ gg-tree-view/script.js
@@ -28,18 +28,35 @@ angular.module('gg').controller('demoController', function($scope) {
     //     // {"id": "5", "displayText": "Entry 5"}
     //     ];
         
+    // $scope.treeData = [
+    //     {"id": "1", "displayText": "Entry 1", "checkedState": "unchecked", "isExpanded": true, pid: null},
+    //     {"id": "2", "displayText": "Entry 2", "isSelected": false, pid: null},
+    //     {"id": "3", "displayText": "Entry 3", pid: null},
+    //     {"id": "4", "displayText": "Entry 4", "isExpanded": true, pid: "1"},
+    //     {"id": "5", "displayText": "Entry 5", "checkedState": "checked", pid: "4"},
+    //     {"id": "6", "displayText": "Entry 6", "checkedState": "checked", pid: "4"},
+    //     {"id": "7", "displayText": "Entry 7", "checkedState": "checked", pid: "6"},
+    //     {"id": "8", "displayText": "Entry 8", "checkedState": "checked", pid: "6"},
+    //     {"id": "9", "displayText": "Entry 9", "checkedState": "unchecked", pid: "6"},
+    //     {"id": "10", "displayText": "Entry 10", "checkedState": "checked", pid: "6"},
+    //     {"id": "11", "displayText": "Entry 11", "checkedState": "checked", pid: "6"},
+    //     {"id": "12", "displayText": "Entry 12", "checkedState": "checked", pid: "11"}
+    // ];
+        
     $scope.treeData = [
-        {"id": "1", "displayText": "Entry 1", "checkedState": "unchecked", "isExpanded": true, pid: null},
-        {"id": "2", "displayText": "Entry 2", "isSelected": false, pid: null},
-        {"id": "3", "displayText": "Entry 3", pid: null},
-        {"id": "4", "displayText": "Entry 4", pid: "1"},
-        {"id": "5", "displayText": "Entry 5", "checkedState": "checked", pid: "4"},
-        {"id": "6", "displayText": "Entry 6", "checkedState": "checked", pid: "4"},
-        {"id": "7", "displayText": "Entry 7", "checkedState": "checked", pid: "6"},
-        {"id": "8", "displayText": "Entry 8", "checkedState": "checked", pid: "6"},
-        {"id": "9", "displayText": "Entry 9", "checkedState": "checked", pid: "6"},
-        {"id": "10", "displayText": "Entry 10", "checkedState": "checked", pid: "6"},
-        {"id": "11", "displayText": "Entry 11", "checkedState": "checked", pid: "6"},
-        {"id": "12", "displayText": "Entry 12", "checkedState": "checked", pid: "11"}
+        {"id": "0", "displayText": "Select All", "isExpanded": true, pid: null},
+        {"id": "1", "displayText": "Parent 1", "checkedState": "unchecked", "isExpanded": true, pid: "0"},
+        {"id": "2", "displayText": "Parent 2", "isSelected": false, pid: "0"},
+        {"id": "3", "displayText": "Parent 3", pid: "0"},
+        {"id": "4", "displayText": "Parent 4", "isExpanded": true, pid: "0"},
+        {"id": "5", "displayText": "Child 1", "checkedState": "checked", pid: "1"},
+        {"id": "6", "displayText": "Child 2", "checkedState": "checked", pid: "1"},
+        {"id": "7", "displayText": "Child 3", "checkedState": "checked", pid: "1"},
+        {"id": "8", "displayText": "Child 1", "checkedState": "checked", pid: "2"},
+        {"id": "9", "displayText": "Child 2", "checkedState": "checked", pid: "2"},
+        {"id": "10", "displayText": "Child 3", "checkedState": "checked", pid: "2"},
+        {"id": "11", "displayText": "Child 1", "checkedState": "checked", pid: "3"},
+        {"id": "12", "displayText": "Child 2", "checkedState": "checked", pid: "3"},
+        {"id": "13", "displayText": "Child 3", "checkedState": "checked", pid: "3"}
     ];
 });
--- gg-tree-view/treeViewService.js
+++ gg-tree-view/treeViewService.js
@@ -1,6 +1,6 @@
 angular.module('gg.directives')
     .service("treeViewService", function() {
-        
+
         var self = this;
 
         var icons = {
@@ -10,7 +10,7 @@ angular.module('gg.directives')
             mixedCheckbox: "fa-dot-circle-o",
             uncheckedCheckbox: "fa-circle-o"
         };
-        
+
         var checkedStates = {
             checked: "checked",
             mixed: "mixed",
@@ -21,6 +21,8 @@ angular.module('gg.directives')
 
         this.init = function(treeNodes) {
             allNodes = treeNodes;
+            var rootNodes = this.getRootNodes();
+            applyMixedStatuses(rootNodes);
         }
 
         this.getRootNodes = function() {
@@ -31,6 +33,53 @@ angular.module('gg.directives')
                 }
             });
             return rootNodes;
+        }
+
+        var applyMixedStatuses = function(nodes) {
+            $.each(nodes, function() {
+                // If there are grandchildren, go down a level
+                if (hasGrandChildrenNodes(this) === true) {
+                    var childNodes = self.getChildNodes(this);
+                    applyMixedStatuses(childNodes);
+                    console.log("has grand children",this.displayText);
+                }
+                else if (hasChildNodes(this) === true) {
+                    console.log("has children, but isn't grand parent",this.displayText);
+                    this.checkedState = getStateOfDescendantNodes(this);
+                    updateStateOfParent(this, this.checkedState);
+                }
+            });
+        }
+
+        var hasGrandChildrenNodes = function(node) {
+            
+            // Each child node
+                // E
+            
+            var grandChildren = 0;
+            var childNodes = self.getChildNodes(node);
+            $.each(childNodes, function() {
+                if (hasChildNodes(this)) {
+                    //var childNodes = self.getChildNodes(this);
+                    grandChildren++;
+                    // $.each(childNodes, function() {
+                    //     if (hasChildNodes(this)) {
+                    //         grandChildren++;
+                    //     }
+                    // });
+                }
+            });
+            return (grandChildren > 0);
+        }
+
+        var hasChildNodes = function(parentNode) {
+            var children = 0;
+            $.each(allNodes, function() {
+                if (typeof this.pid !== "undefined" && this.pid === parentNode.id) {
+                    children++;
+                }
+            })
+            return (children > 0);
         }
 
         this.getChildNodes = function(parentNode) {
@@ -65,86 +114,79 @@ angular.module('gg.directives')
         }
 
         var deselectAllNodes = function(nodes) {
-            $.each(allNodes,function() {
+            $.each(allNodes, function() {
                 this.isSelected = false;
             });
-        }
-
-        var hasChildNodes = function(parentNode) {
-            var children = 0;
-            $.each(allNodes, function() {
-                if (typeof this.pid !== "undefined" && this.pid === parentNode.id) {
-                    children++;
-                }
-            })
-            return (children > 0);
         }
 
         this.toggleCheckbox = function(node) {
             var newCheckedState = toggleCheckedState(node);
             node.checkedState = newCheckedState;
             if (hasChildNodes(node)) {
-                this.applyStateToChildNodes(node,newCheckedState);
+                this.applyStateToChildNodes(node, newCheckedState);
             }
             if (typeof node.pid !== "undefined" && node.pid !== null) {
-                updateStateOfParent(node,newCheckedState);
+                updateStateOfParent(node, newCheckedState);
             }
         }
-        
+
         var toggleCheckedState = function(node) {
             if (node.checkedState === checkedStates.checked) {
                 return node.checkedState = checkedStates.unchecked;
-            } else if (typeof node.checkedState === "undefined" || node.checkedState === "" || node.checkedState === checkedStates.unchecked) {
+            }
+            else if (typeof node.checkedState === "undefined" || node.checkedState === "" || node.checkedState === checkedStates.unchecked) {
                 return node.checkedState = checkedStates.checked;
             }
         }
-        
-        this.applyStateToChildNodes = function(parentNode,newCheckedState) {
+
+        this.applyStateToChildNodes = function(parentNode, newCheckedState) {
             $.each(this.getChildNodes(parentNode), function() {
                 if (hasChildNodes(this)) {
-                    self.applyStateToChildNodes(this,newCheckedState);
+                    self.applyStateToChildNodes(this, newCheckedState);
                 }
                 this.checkedState = newCheckedState;
             })
         }
-        
-        var updateStateOfParent = function(childNode,newCheckedState) {
+
+        var updateStateOfParent = function(childNode, newCheckedState) {
             var parentNode = getParentNode(childNode);
             if (typeof parentNode !== "undefined") {
                 var stateOfChildNodes = getStateOfDescendantNodes(parentNode);
                 var originalParentNodeState = parentNode.checkedState;
                 parentNode.checkedState = stateOfChildNodes;
-                if (originalParentNodeState !== parentNode.checkedState) {
-                    updateStateOfParent(parentNode,parentNode.checkedState);
-                }
+                //if (originalParentNodeState !== parentNode.checkedState) {
+                    updateStateOfParent(parentNode, parentNode.checkedState);
+                //}
             }
         }
-        
+
         var getParentNode = function(childNode) {
             var parentNode;
-            $.each(allNodes,function() {
+            $.each(allNodes, function() {
                 if (this.id === childNode.pid) {
                     parentNode = this;
                 }
             });
             return parentNode;
         }
-        
+
         var getStateOfDescendantNodes = function(parentNode) {
             var numberOfDescendantNodes = getNumberOfDescendantNodes(parentNode);
             var numberOfCheckedDescendantNodes = getNumberOfCheckedDescendantNodes(parentNode);
             if (numberOfCheckedDescendantNodes === 0) {
                 return checkedStates.unchecked;
-            } else if (numberOfCheckedDescendantNodes > 0 && numberOfCheckedDescendantNodes < numberOfDescendantNodes) {
+            }
+            else if (numberOfCheckedDescendantNodes > 0 && numberOfCheckedDescendantNodes < numberOfDescendantNodes) {
                 return checkedStates.mixed;
-            } else if (numberOfCheckedDescendantNodes === numberOfDescendantNodes) {
+            }
+            else if (numberOfCheckedDescendantNodes === numberOfDescendantNodes) {
                 return checkedStates.checked
             }
         }
-        
+
         var getNumberOfDescendantNodes = function(parentNode) {
             var count = 0;
-            $.each(allNodes,function() {
+            $.each(allNodes, function() {
                 if (this.pid === parentNode.id) {
                     count += getNumberOfDescendantNodes(this);
                     count++;
@@ -152,10 +194,10 @@ angular.module('gg.directives')
             })
             return count;
         }
-        
+
         var getNumberOfCheckedDescendantNodes = function(parentNode) {
             var count = 0;
-            $.each(allNodes,function() {
+            $.each(allNodes, function() {
                 if (this.pid === parentNode.id) {
                     count += getNumberOfCheckedDescendantNodes(this);
                 }
@@ -169,9 +211,11 @@ angular.module('gg.directives')
         this.getCheckboxIcon = function(node) {
             if (node.checkedState === checkedStates.checked) {
                 return icons.checkedCheckbox;
-            } else if (node.checkedState === checkedStates.mixed) {
+            }
+            else if (node.checkedState === checkedStates.mixed) {
                 return icons.mixedCheckbox;
-            } else {
+            }
+            else {
                 return icons.uncheckedCheckbox;
             }
         }
